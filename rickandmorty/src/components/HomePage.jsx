2dacb64f3932e6a5ed42940dbd989c917d0ee1d2
Beril Kartal <berilk.dev@gmail.com>
2024-12-13 14:16:12 +0300

    create home page view

--- rickandmorty/src/components/HomePage.jsx
+++ rickandmorty/src/components/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, Typography, Box } from '@mui/material';
+import rickAndMortyIcon from '../assets/rick-and-morty-icon.png';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -9,11 +11,37 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to the Rick and Morty Universe !</h1>
-      <p>Click to start exploring.</p>
-      <button onClick={handleNavigateToHome}>Explore</button>
-    </div>
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'secondary.main',
+      }}
+    >
+      <Box
+        component="img"
+        src={rickAndMortyIcon} 
+        alt="Rick and Morty Icon"
+        sx={{
+          width: 400, 
+          height: 'auto', 
+          mb: 5,
+        }}
+      />
+      
+      <Typography variant="h3">Rick and Morty Universe</Typography>
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        Click to start explore
+      </Typography>
+      
+      <Button variant="contained" color="secondary" onClick={handleNavigateToHome}>
+        Explore
+      </Button>
+    </Box>
   );
 };
 
